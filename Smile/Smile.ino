7e6f6138c5041c8a1df2417155564972408eb4c6
Yuri Lima <y.m.lima19@gmail.com>
2015-07-10 16:13:35 -0300

    Refatorado

--- Smile/Smile.ino
+++ Smile/Smile.ino
@@ -67,70 +67,65 @@ public:
 #define SetPoint 20 //Seta em 10 Cm
 //==========================================================
 //PID construtor 
-PID meuPid1(3.2, 0.00, 0.05);
-PID meuPid2(3.2, 0.00, 0.05);
+PID meuPid1(20, 0.02, 0.05);
+PID meuPid2(3.2, 0.02, 0.05);
 //========================================================== 
 //Servo
 #include <Servo.h>
 Servo myservo1;
 Servo myservo2; 
-
-int controlePwm1 = 45;//90;//direita parado e quando diminui  ele vai pra frente e aumenta a velocidade
-int controlePwm2 = 131;//176;//esquerda parado e quando aumetar ele vai pra frente e aumentar a velocidade 86+45
+#define direita 1468//90
+#define esquerda 1436//86
+int controlePwm1 = direita;//90;//direita parado e quando diminui  velocidade
+int controlePwm2 = esquerda;//86;//esquerda parado e quando aumetar ele vai pra frente e aumentar a velocidade 86+45
 
 void setup() {
   Serial.begin(9600);
-  myservo1.attach(12);//direito 
-  myservo2.attach(11);//esquerdo
+  //myservo1.attach(12);//direito
+ // myservo2.attach(11);//esquerdo
   pinMode(echoPin, INPUT); // define o pino 13 como entrada (recebe)
   pinMode(trigPin, OUTPUT); // define o pino 12 como saida (envia)
   pinMode(RED, 1);pinMode(GREEN, 1);pinMode(BLUE, 1);
   meuPid1.setSetPoint(SetPoint);
-  meuPid2.setSetPoint(SetPoint);
+  meuPid2.setSetPoint(SetPoint);                                                                                                                                                                                  
 }
-
-
-
 void loop() {
- 
-  
-  int diT,flag;
+  int diT,flag;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
     flag=1;
     while(distancia()<40){
       if(flag=1){
-        myservo1.attach(12);//direito 
+        myservo1.attach(12);//direito  
         myservo2.attach(11);//esquerdo
-        Serial.print(meuPid1.process());Serial.print(" - ");Serial.print(meuPid2.process());Serial.print(" - ");Serial.println(diT);
+        Serial.print(meuPid1.process());Serial.print(" - ");Serial.println(diT);
       }flag=0;
       diT=distancia(); 
       // Manda as amostra de leitura da distancia para o objeto PID!
       //Serial.println(diT);
       meuPid1.addNewSample(diT);
-      meuPid2.addNewSample(diT);
+      //meuPid2.addNewSample(diT);
       // Converte para controle
-      controlePwm1 = ((meuPid1.process() + controlePwm1)+45);//Ajuste tecnico pre sono
-      controlePwm2 = (-(meuPid2.process() - controlePwm2)-45);//Ajuste tecnico pre sono
-      //Serial.print( controlePwm1);Serial.print(" - ");Serial.print( controlePwm2);Serial.print(" - ");Serial.println(diT);
-      //controlePwm1=map(controlePwm1,90,controlePwm1,SetPoint,40);//int controlePwm1 = 0;//direita parado e quando diminui  ele vai pra frente e aumenta a velocidade
-      //controlePwm2=map(controlePwm2,86,controlePwm2,SetPoint,40);//int controlePwm2 = 200;//esquerda parado e quando aumetar ele vai pra frente e aumentar a velocidade
-      //Serial.print(meuPid1.process());Serial.print(" - ");Serial.print(meuPid2.process());Serial.print(" - ");Serial.println(diT);
+      int teste=meuPid1.process();
+      controlePwm1 = (teste + controlePwm1);//Direita
+      controlePwm2 = (-(teste - controlePwm2)+10);//Esquerda
       // Intervalo para desativar os pinos do servo
-      if((controlePwm1<96 && controlePwm1>84) || (controlePwm2<92 && controlePwm2>80)){
-        //if((diT<23) && (diT>18)){
+      //if((controlePwm1<96 && controlePwm1>84) || (controlePwm2<92 && controlePwm2>80)){
+     /* while(distancia()<25 && distancia()>15){
         digitalWrite(BLUE,1);  
         myservo1.detach();//direito 
         myservo2.detach();//esquerdo
-        controlePwm1=45;
-        controlePwm2=131;
-      }
-      else{
+        controlePwm1=direita;
+        controlePwm2=esquerda;
+      }*/
+        myservo1.attach(12);//direito  
+        myservo2.attach(11);//esquerdo
+     
         digitalWrite(BLUE,0);
-        myservo1.write(controlePwm1);// direita
-        myservo2.write(controlePwm2);//esqerda
+         myservo1.writeMicroseconds(controlePwm1);// direita quanto menor maior velocidade
+         myservo2.writeMicroseconds(controlePwm2);// esquerda
+        controlePwm1=direita;
+        controlePwm2=esquerda;
         (controlePwm1<90) || (controlePwm2>86)?digitalWrite(GREEN,1):digitalWrite(RED,1);
-        controlePwm1=45;
-        controlePwm2=131;
-     }
+     
     }
       digitalWrite(RED,0);
       digitalWrite(BLUE,0);
