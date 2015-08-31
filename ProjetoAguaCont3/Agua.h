de72b66582c42c30704c79f17b5f91efc513c006
Yuri Lima <y.m.lima19@gmail.com>
2015-08-31 07:06:15 -0300

    Refatorados

---
+++ ProjetoAguaCont3/Agua.h
@@ -0,0 +1,21 @@
+#ifndef AGUA_H//Essas diretivas não deixarão as declarações/definições da biblioteca serem inseridas mais de uma vez em um projeto.
+#define AGUA_H
+ 
+#include <Arduino.h>
+
+class ULTRA{
+public: 
+//Ultrassom
+//=============================================================================================================  
+  ULTRA( int _echoPin, int _trigPin);//Recebe os parametros das constantes
+  int distancia();//Recebe as amostras lidas de algo Exemplo.: distancia, temperatura...
+//=============================================================================================================
+private:
+//Ultrasson
+//=============================================================================================================
+ int echoPin;
+ int trigPin;
+ double duracao=0;
+ double disT=0;
+};
+#endif
