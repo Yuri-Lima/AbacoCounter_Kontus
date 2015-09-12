5b08d800cc0aac7723922a69626cbc7a7521b1b1
Yuri Lima <y.m.lima19@gmail.com>
2015-09-12 17:21:09 -0300

    Refatorado

--- ProjetoAnimaisFalantes_01/ProjetoAnimaisFalantes_01.ino
+++ ProjetoAnimaisFalantes_01/ProjetoAnimaisFalantes_01.ino
@@ -1,117 +1,106 @@
-  
+
 //Programa : Modulo WTV020-SD e Arduino Uno
 //Autor : Arduino e Cia
- 
-#include <Wtv020sd16p.h>
-#include <Servo.h> 
-Servo myservo; 
-byte pos= 0, pos2=0;
 
-#define tempMusic 120000
-int resetPin = 4;  //Pino Reset
-int clockPin = 21;  //Pino clock
-int dataPin = 20;   //Pino data (DI)
-int busyPin = 7;   //Pino busy
- 
+#include <Wtv020sd16p.h>
+#include <Servo.h>
+Servo myservo,myservo2;
+int pos = 0,pos2=0;
+
+#define echoPin 2
+#define trigPin 3
+double duracao = 0;
+double disT = 0;
+boolean flag = 0;
+#define tempMusic 3000
+# define resetPin 4  //Pino Reset
+#define clockPin A5  //Pino clock
+#define dataPin A4   //Pino data (DI)
+int busyPin = 5;   //Pino busy
+long previousMillis = 0;        // Variável de controle do tempo
+long redLedInterval = 10000;     // Tempo em ms do intervalo a ser executado
+long previousMillis2 = 0;        // Variável de controle do tempo
+long redLedInterval2 = 12000;     // Tempo em ms do intervalo a ser executado
+
 //Variavel que armazena os caracteres recebidos
 char buf;
 
-Wtv020sd16p wtv020sd16p(resetPin,clockPin,dataPin,busyPin);
- 
-void setup(){
-  myservo.attach(9);
+Wtv020sd16p wtv020sd16p(resetPin, clockPin, dataPin, busyPin);
+
+void setup() {
+  myservo.attach(6);
+  myservo2.attach(7);
   Serial.begin(9600);
   wtv020sd16p.reset();
+  myservo.write(0);
   menu_inicial();
 }
- 
-void loop(){
-  for(pos = 0, pos2=0; pos <= 180, pos2 <= 180; pos += 1, pos2 += 1) { myservo.write(pos); myservo.write(pos2); delay(15);} 
-  for(pos = 180, pos2 = 180; pos>=0, pos2>=0; pos-=1, pos2-=1) { myservo.write(pos); myservo.write(pos2); delay(15);}
-  
-  while(Serial.available() > 0){
-    buf = Serial.read();
-     if (buf == '0'){
-      Serial.println("Reproduzindo 0001.ad4");
-      wtv020sd16p.playVoice(0);
-      //Reproduz o arquivo 1
-      wtv020sd16p.asyncPlayVoice(0);
-      delay(tempMusic);
-      wtv020sd16p.stopVoice();
-      delay(2000);
-      menu_inicial();  
+
+void loop() {
+  //==========================================================================
+  //Asa
+  unsigned long currentMillis = millis();    //Tempo atual em ms
+  if (currentMillis - previousMillis > redLedInterval) {
+    previousMillis = currentMillis;    
+    for (pos = 0; pos <= 50; pos += 1) { 
+      myservo.write(pos);              
+      delay(5);                       
     }
-    if (buf == '1'){
-      Serial.println("Reproduzindo 0002.ad4");
-      wtv020sd16p.playVoice(1);
-      //Reproduz o arquivo 1
-      wtv020sd16p.asyncPlayVoice(1);
-      delay(tempMusic);
-      wtv020sd16p.stopVoice();
-      delay(2000);
-      menu_inicial();  
-    }
-     
-    //Caso seja recebido o numero 2, reproduz o arquivo 0002.ad4
-    if (buf == '2')
-    {
-      Serial.println("Reproduzindo 0002.ad4");
-      wtv020sd16p.playVoice(2);
-      //Reproduz o arquivo 2
-      wtv020sd16p.asyncPlayVoice(2);
-      delay(tempMusic);
-      wtv020sd16p.stopVoice();
-      delay(2000);
-      menu_inicial();
-    }
-    if (buf == '3')
-    {
-      Serial.println("Reproduzindo 0002.ad4");
-      wtv020sd16p.playVoice(3);
-      //Reproduz o arquivo 2
-      wtv020sd16p.asyncPlayVoice(3);
-      delay(tempMusic);
-      wtv020sd16p.stopVoice();
-      delay(2000);
-      menu_inicial();
-    }
-    if (buf == '4')
-    {
-      Serial.println("Reproduzindo 0002.ad4");
-      wtv020sd16p.playVoice(4);
-      //Reproduz o arquivo 2
-      wtv020sd16p.asyncPlayVoice(4);
-      delay(tempMusic);
-      wtv020sd16p.stopVoice();
-      delay(2000);
-      menu_inicial();
-    }
-    if (buf == '5')
-    {
-      Serial.println("Reproduzindo 0002.ad4");
-      wtv020sd16p.playVoice(5);
-      //Reproduz o arquivo 2
-      wtv020sd16p.asyncPlayVoice(5);
-      delay(tempMusic);
-      wtv020sd16p.stopVoice();
-      delay(2000);
-      menu_inicial();
-    }
-     if (buf == 'r')
-    {
-      Serial.println("Reset");
-      wtv020sd16p.stopVoice();
-      delay(2000);
-      menu_inicial();  
+    for (pos = 50; pos >= 0; pos -= 1) {
+      myservo.write(pos);              
+      delay(5);                       
     }
   }
+  //==========================================================================
+   //==========================================================================
+  //Boca
+   unsigned long currentMillis2 = millis();    //Tempo atual em ms
+  if (currentMillis2 - previousMillis2 > redLedInterval2) {
+    previousMillis2 = currentMillis2;    
+    for (pos2 = 0; pos2 <= 70; pos2 += 1){ // in steps of 1 degree
+      myservo2.write(pos2);              
+      delay(5);                       
+    }
+    for (pos2 = 70; pos2 >= 0; pos2 -= 1) {
+      myservo2.write(pos2);              
+      delay(5);                       
+    }
+  }
+  if (distancia() < 20) {
+    Serial.println("Reproduzindo 0001.ad4");
+    wtv020sd16p.playVoice(1);
+    //Reproduz o arquivo 1
+    wtv020sd16p.asyncPlayVoice(0);
+    delay(tempMusic);
+    wtv020sd16p.stopVoice();
+    delay(20);
+    menu_inicial();
+  }
 }
- 
+
+
 //Mostra menu de opcoes
 void menu_inicial()
 {
   Serial.println("\nDigite : ");
   Serial.println("1 - Reproduz o arquivo 0001.ad4");
-  Serial.println("2 - Reproduz o arquivo 0002.ad4");
   Serial.println();
+}
+int distancia() {
+  //seta o pino 12 com um pulso baixo "LOW" ou desligado ou ainda 0
+  digitalWrite(trigPin, LOW);
+  // delay de 2 microssegundos
+  delayMicroseconds(2);
+  //seta o pino 12 com pulso alto "HIGH" ou ligado ou ainda 1
+  digitalWrite(trigPin, HIGH);
+  //delay de 10 microssegundos
+  delayMicroseconds(10);
+  //seta o pino 12 com pulso baixo novamente
+  digitalWrite(trigPin, LOW);
+  duracao = pulseIn(echoPin, HIGH, 4000);
+  disT = duracao / 58;
+  if (disT == 0) {
+    disT = 999;//3E7->999
+  }
+  return disT;
 }
